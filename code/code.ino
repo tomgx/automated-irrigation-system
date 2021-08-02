603aa21b7057b90861f9ba3f959d20b11a90b8d4
tomgx <tomgxoriginal@hotmail.com>
2021-08-02 18:29:49 +0100

    1

---
+++ code/code.ino
@@ -0,0 +1,14 @@
+// if the moisture in the soil is less than this number, activate water pump
+const int dry = 270;
+// D12 pin used for water pump
+const int waterPump = 12;
+// A4 pin used for moisture sensor
+const int moistureSensor = A4;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
