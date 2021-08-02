6d92ac7369717f3eef82e2f661252216376e393a
tomgx <tomgxoriginal@hotmail.com>
2021-08-02 18:42:55 +0100

    update

--- code/code.ino
+++ code/code.ino
@@ -1,5 +1,5 @@
 // if the moisture in the soil is less than this number, activate water pump
-const int dry = 270;
+const int drySoil = 270;
 // D12 pin used for water pump
 const int waterPump = 12;
 // A4 pin used for moisture sensor
@@ -8,9 +8,21 @@ const int moistureSensor = A4;
 void setup() {
   pinMode(waterPump, OUTPUT);
   pinMode(moistureSensor, INPUT);
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  
 }
 
 void loop() {
+  int moistureReading = analogRead(moistureSensor);
+     Serial.println(moistureReading);
+     delay(3000);
 
+     if (moistureReading >= drySoil){
+      Serial.println("Watering plant(s) for 2 seconds. Moisture level is currently: " + String(moistureReading));
+      digitalWrite(waterPump, LOW);
+      delay(2000);
+      //water for 2 seconds
+     }else{
+      digitalWrite(waterPump, HIGH);
+     }
 }
