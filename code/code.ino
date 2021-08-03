8db606d5958a96b801bd9de985e262e239544886
tomgx <tomgxoriginal@hotmail.com>
2021-08-03 01:03:08 +0100

    if moistureReading is good for 5 times. Sleep for 1 Day
    So rather than check to see if the moisture is at an adequate level every second. It checks to see if it is stable 5 times within the set amount of time. It is then put to sleep for 1 Day if it the moisture is stable. After 1 day it wakes up and the process repeats. If the moisture = dry, the water pump will activatate.

--- code/code.ino
+++ code/code.ino
@@ -1,9 +1,15 @@
 // if the moisture in the soil is less than this number, activate water pump
-const int drySoil = 610;
+const int drySoil = 500;
 // D12 pin used for water pump
 const int waterPump = 12;
 // A0 pin used for moisture sensor
 const int moistureSensor = A0;
+
+//delay: 1 second, 1 minute, 1 hours, 1 day. 
+const long oneSecond = 1000;
+const long oneMinute = oneSecond * 60;
+const long oneHour   = oneMinute * 60;
+const long oneDay    = oneHour * 24;
 
 void setup() {
   pinMode(waterPump, OUTPUT);
@@ -11,21 +17,45 @@ void setup() {
   Serial.begin(9600);  
 }
 
+void delayOneDay() {
+  Serial.println("Soil moisture is good, next check in: 1 day");
+        delay(oneDay);
+}
+
 void loop() {
   int moistureReading = analogRead(moistureSensor);
      Serial.println(moistureReading);
-     delay(1000);
-
+     delay(100);
+//if moisture reading is greater than or equal to 600
      if (moistureReading >= drySoil){
       Serial.println("Watering plant(s) for 2 seconds. Moisture level is currently: " + String(moistureReading));
       //turn on water pump
-      digitalWrite(waterPump, HIGH);
+      digitalWrite(waterPump, LOW);
       //water for 2 seconds
       delay(2000);
-      
+      digitalWrite(waterPump, HIGH);
+      Serial.println("Done watering.");
      }else{
+      
       //turn off water pump
-      digitalWrite(waterPump, LOW);
-      Serial.println("Soil moisture level is good: " + String(moistureReading));
+      digitalWrite(waterPump, HIGH);
+
+      int i = 0;
+      while (i < 5){
+        delay(5000);
+        int goodMoisture = moistureReading < drySoil;
+        if (goodMoisture) {
+          Serial.println("Soil moisture level is good: " + String(moistureReading));
+          delay(oneSecond);
+          int moistureReading = analogRead(moistureSensor);
+          Serial.println(moistureReading);
+          if (moistureReading >= drySoil){
+            break;
+          }
+          i++;}while(i == 5) {
+            delayOneDay();
+          }
+          }
+        
      }
 }
